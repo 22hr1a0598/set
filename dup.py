3b61e13a3d7787eff8e432f6d4488093fb363a95
22hr1a0598 <154883815+22hr1a0598@users.noreply.github.com>
2024-10-15 11:12:25 +0530

    Create dup.py

---
+++ dup.py
@@ -0,0 +1,25 @@
+'''
+Write a program to get set values in a single line with space(including duplicate values) and find the number of duplicate values given during input and print the output set value without duplicate elements.
+Sample Input:
+6
+1
+2
+1
+2
+3
+1
+Sample Output:
+Duplicate Values: 3
+1 2 3 
+
+'''
+n = int(input("Enter the number of values: "))
+values_list = []
+unique_values = set()
+for _ in range(n):
+    value = int(input())
+    values_list.append(value)
+    unique_values.add(value)
+    duplicate_count = len(values_list) - len(unique_values)
+print(f"Duplicate Values: {duplicate_count}")
+print(' '.join(map(str, sorted(unique_values))) + ' ')
